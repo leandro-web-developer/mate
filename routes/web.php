db5a5a2d544148ed80ea1ef1e92293f72eabf158
gn360 <desarrolloweb@gn360.com.uy>
2021-09-13 12:13:44 -0300

    editando codigo desde php

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,4 @@ Route::get('/', function () {
 
 Route::post('enviar', [App\Http\Controllers\EmailController::class, 'store']);
 
-Route::get('fopen', 'EditorController@elPais');
+Route::get('fopen', [App\Http\Controllers\EmailController::class, 'elPais');
