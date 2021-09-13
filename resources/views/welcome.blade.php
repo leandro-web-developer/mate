abcb2dd07b443e32734f9c8a6944349abc98de6e
gn360 <desarrolloweb@gn360.com.uy>
2021-09-13 09:03:01 -0300

    mostrando el pais v1.2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -59,7 +59,9 @@
                                 foreach ($arr_elpais as $i) {
                                     echo "<div class='col-12 py-2'>";
                                     echo "<div class='row'>";
-                                    echo "<div class='col-6'><img src='{$i['i']}' class='img-responsive' alt='{$i['t']}'></div>";
+                                    if ($i['i']) {
+                                        echo "<div class='col-6'><img src='{$i['i']}' class='img-responsive' alt='{$i['t']}'></div>";
+                                    }
                                     echo "<div class='col-6'><a href='{$i['u']}' target='_blank'>{$i['t']}</a></div>";
                                     echo '</div>';
                                     echo '</div>';
