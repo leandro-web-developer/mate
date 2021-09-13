d19e3816757b5b32958e2f2b34a94196f83e6eb6
gn360 <desarrolloweb@gn360.com.uy>
2021-09-13 15:55:30 -0300

    el observador

--- app/Http/Controllers/EditorController.php
+++ app/Http/Controllers/EditorController.php
@@ -6,17 +6,15 @@ use Illuminate\Http\Request;
 
 class EditorController extends Controller
 {
+    /**
+     * 
+     */
     public static function elPais()
     {
         // EL PAIS -----------
-        header('content-type:text/plain');
+
         $url = 'https://www.elpais.com.uy';
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        $html = curl_exec($ch);
-        $html = preg_replace('!\n!', '', $html);
-        curl_close($ch);
+        $html = Self::curl($url);
 
         // BUSCO ARTICULOS -----
         if (preg_match_all('!<article[^>]*>(.*?)</article>!iu', $html, $m)) {
@@ -41,113 +39,92 @@ class EditorController extends Controller
             }
 
             if (count($arr_elpais) > 0) {
-                $elpais = "<H1 class='py-4' id='noticas-el-pais'>NOTICIAS DE DIARIO EL PAÍS</H1>";
-                $elpais .= "<div class='row'>";
-                $elpais .= "  <div class='col-12 py-2'>";
-                $elpais .= "      <div class='row'>";
-                foreach ($arr_elpais as $i) {
-                    if (array_key_exists('u', $i)) {
-                        $elpais .= "       <div class='col-6 col-sm-4 col-md-3 py-2'>";
-                        $elpais .= "          <div class='card-sl noticia'>";
-                        $elpais .= "              <div class='card-image'>";
-                        if (array_key_exists('i', $i)) {
-                            $elpais .= "              <img src='{$i['i']}' alt='{$i['t']}' />";
-                        } else {
-                            $elpais .= "              <img src='" . asset('img/no-image.svg') . "' alt='Mate.uy - Sin foto' />";
-                        }
-                        $elpais .= '              </div>';
-                        $elpais .= "              <div class='card-text'>{$i['t']}</div>";
-                        $elpais .= "              <a href='{$i['u']}' class='card-button'>Ver en El País</a>";
-                        $elpais .= '          </div>';
-                        $elpais .= '       </div>';
-                    }
-                }
-                $elpais .= '      </div>';
-                $elpais .= '  </div>';
-                $elpais .= '</div>';
-
-
+                $elpais = Self::html('EL PAÍS', $arr_elpais);
                 $ourFileName = "/var/www/lea/mate/resources/views/diarios/elpais.blade.php";
                 $ourFileHandle = fopen($ourFileName, 'w');
-
                 fwrite($ourFileHandle, $elpais);
-
                 fclose($ourFileHandle);
             }
         }
     }
 
-
+    /**
+     * 
+     */
     public static function elObservador()
     {
         // EL OBSERVADOR -----------
-        header('content-type:text/plain');
         $url = 'https://www.elobservador.com.uy';
+        $html = Self::curl($url);
+
+        // BUSCO ARTICULOS -----
+        if (preg_match_all('!<div class="\s*linea[^>]>\s*<a href="([^"]+)">\s*<div class="imagen.*?">\s*<figure class="figure">.*?<img .*?data-src="([^"]+)".*?</a>.*?<h3 class="volanta .*?">.*?title="([^"]+)">.*?</a>\s*</h3>\s*<h2.*?>.*?>([^<]+)</a>!iu', $html, $m)) {
+            return $m;
+
+            $arr_elobservador = [];
+            for ($i = 0; $i < count($m[0]); $i++) {
+                // EN CADA ARTICULO BUSCO URL Y TITULO -----
+                preg_match('!<div class="\s*linea[^>]>\s*<a href="([^"]+)">\s*<div class="imagen.*?">\s*<figure class="figure">.*?<img .*?data-src="([^"]+)".*?</a>.*?<h3 class="volanta .*?">.*?title="([^"]+)">.*?</a>\s*</h3>\s*<h2.*?>.*?>([^<]+)</a>!iu', $m[0][$i], $u);
+                $x = [];
+                $x['u'] = $url . $u[1];
+                $x['i'] = $u[2];
+                $x['t'] = $u[3] . ' - ' . $u[4];
+                $arr_elobservador[] = $x;
+            }
+
+            if (count($arr_elobservador) > 0) {
+                $elobservador = Self::html('EL OBSERVADOR', $arr_elobservador);
+                $ourFileName = "/var/www/lea/mate/resources/views/diarios/elobservador.blade.php";
+                $ourFileHandle = fopen($ourFileName, 'w');
+                fwrite($ourFileHandle, $elobservador);
+                fclose($ourFileHandle);
+            }
+        }
+    }
+
+    // ----------------------------------------------------
+
+    private static function curl($url)
+    {
+        header('content-type:text/plain');
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         $html = curl_exec($ch);
         $html = preg_replace('!\n!', '', $html);
         curl_close($ch);
+        return $html;
+    }
 
-        // BUSCO ARTICULOS -----
-        if (preg_match_all('!<div class="\s*linea[^>]>\s*<a href="([^"]+)">\s*<div class="imagen.*?">\s*<figure class="figure">.*?<img .*?data-src="([^"]+)".*?</a>.*?<h3 class="volanta .*?">.*?title="([^"]+)">.*?</a>\s*</h3>\s*<h2.*?>.*?>([^<]+)</a>!iu', $html, $m)) {
-            return $m;
-
-            $arr_elpais = [];
-            for ($i = 0; $i < count($m[0]); $i++) {
-                // EN CADA ARTICULO BUSCO URL Y TITULO -----
-                if (preg_match('!asdaadasdasdasda!iu', $m[0][$i], $h)) {
-                    // $uri = $url . $h[1];
-                    // // SOLO AGREGO AL ARRAY GENERAL SI NO EXISTE YA EN EL MISMO -----
-                    // if (!array_search($uri, array_column($arr_elpais, 'u'))) {
-                    //     $x = [];
-                    //     $x['u'] = $uri;
-                    //     $x['t'] = $h[2];
-
-                    //     // SI ENCONTRE LO DEMAS ENTONCES BUSCO LA IMAGEN -----
-                    //     if (preg_match('!<img[^>]*src="([^"]+)"!iu', $m[0][$i], $img)) {
-                    //         $x['i'] = $img[1];
-                    //     }
-                    //     $arr_elpais[] = $x;
-                    // }
+    /**
+     * 
+     */
+    private static function html($nombre, $arr)
+    {
+        $diario = "<H1 class='py-4' id='noticas-el-pais'>NOTICIAS DE DIARIO {$nombre}</H1>";
+        $diario .= "<div class='row'>";
+        $diario .= "  <div class='col-12 py-2'>";
+        $diario .= "      <div class='row'>";
+        foreach ($arr as $i) {
+            if (array_key_exists('u', $i)) {
+                $diario .= "       <div class='col-6 col-sm-4 col-md-3 py-2'>";
+                $diario .= "          <div class='card-sl noticia'>";
+                $diario .= "              <div class='card-image'>";
+                if (array_key_exists('i', $i)) {
+                    $diario .= "              <img src='{$i['i']}' alt='{$i['t']}' />";
+                } else {
+                    $diario .= "              <img src='" . asset('img/no-image.svg') . "' alt='Mate.uy - Sin foto' />";
                 }
+                $diario .= '              </div>';
+                $diario .= "              <div class='card-text'>{$i['t']}</div>";
+                $diario .= "              <a href='{$i['u']}' class='card-button'>Ver en El País</a>";
+                $diario .= '          </div>';
+                $diario .= '       </div>';
             }
-
-            // if (count($arr_elpais) > 0) {
-            //     $elpais = "<H1 class='py-4' id='noticas-el-pais'>NOTICIAS DE DIARIO EL PAÍS</H1>";
-            //     $elpais .= "<div class='row'>";
-            //     $elpais .= "  <div class='col-12 py-2'>";
-            //     $elpais .= "      <div class='row'>";
-            //     foreach ($arr_elpais as $i) {
-            //         if (array_key_exists('u', $i)) {
-            //             $elpais .= "       <div class='col-6 col-sm-4 col-md-3 py-2'>";
-            //             $elpais .= "          <div class='card-sl noticia'>";
-            //             $elpais .= "              <div class='card-image'>";
-            //             if (array_key_exists('i', $i)) {
-            //                 $elpais .= "              <img src='{$i['i']}' alt='{$i['t']}' />";
-            //             } else {
-            //                 $elpais .= "              <img src='" . asset('img/no-image.svg') . "' alt='Mate.uy - Sin foto' />";
-            //             }
-            //             $elpais .= '              </div>';
-            //             $elpais .= "              <div class='card-text'>{$i['t']}</div>";
-            //             $elpais .= "              <a href='{$i['u']}' class='card-button'>Ver en El País</a>";
-            //             $elpais .= '          </div>';
-            //             $elpais .= '       </div>';
-            //         }
-            //     }
-            //     $elpais .= '      </div>';
-            //     $elpais .= '  </div>';
-            //     $elpais .= '</div>';
-
-
-            //     $ourFileName = "/var/www/lea/mate/resources/views/diarios/elpais.blade.php";
-            //     $ourFileHandle = fopen($ourFileName, 'w');
-
-            //     fwrite($ourFileHandle, $elpais);
-
-                // fclose($ourFileHandle);
-            // }
         }
+        $diario .= '      </div>';
+        $diario .= '  </div>';
+        $diario .= '</div>';
+        return $diario;
     }
 }
