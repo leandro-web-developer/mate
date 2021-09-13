d7eb4bd1e1646aa5aacfbd5689cd90793106e7a5
gn360 <desarrolloweb@gn360.com.uy>
2021-09-13 09:33:03 -0300

    agregue npm

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,10 +12,10 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 
     <!-- Scripts -->
-    <script src="/js/app.js" defer></script>
+    <script src="{{ asset('js/app.js') }}" defer></script>
 
     <!-- Styles -->
-    <link href="/css/app.css" rel="stylesheet">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
     <style>
     </style>
